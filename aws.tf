d1ecc7d860515d63712ad31603277e0ccf3ce600
Vaibhav Chaudhari <134763341+VaibhavVinayakChaudhari@users.noreply.github.com>
2024-05-03 18:11:27 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "allow_ssh_http" {
 resource "aws_instance" "myinstance1" {
   ami = "ami-0b8b44ec9a8f90422"
   instance_type = "t2.micro"
-  key_name = "ohio"
+  key_name = "terraform"
   associate_public_ip_address = true
   subnet_id = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
